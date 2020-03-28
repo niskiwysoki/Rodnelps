901e9c24088907a6c3e26c10694f87dc15aabc06
niskiwysoki <niskiwysoki@gmail.com>
2020-03-28 15:47:19 +0100

    Setup_Git_Ignore

---
+++ RodnelpsGame/Source/Rodnelps/Rodnelps.cpp
@@ -0,0 +1,6 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "Rodnelps.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Rodnelps, "Rodnelps" );
